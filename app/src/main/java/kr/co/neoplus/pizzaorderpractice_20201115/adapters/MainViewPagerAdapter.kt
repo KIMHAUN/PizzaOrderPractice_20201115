3502536123b1daa3244795ac6402bf26dd7d4133
kimhaun <wha02068@gmail.com>
2020-11-15 15:22:45 +0900

    초기 셋팅

--- app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/adapters/MainViewPagerAdapter.kt
@@ -3,13 +3,18 @@ package kr.co.neoplus.pizzaorderpractice_20201115.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import kr.co.neoplus.pizzaorderpractice_20201115.fragments.MyInfo
+import kr.co.neoplus.pizzaorderpractice_20201115.fragments.PizzaOrder
 
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm){
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return 2
     }
 
     override fun getItem(position: Int): Fragment {
-        TODO("Not yet implemented")
+        return when(position){
+            0-> PizzaOrder()
+            else-> MyInfo()
+        }
     }
 }
