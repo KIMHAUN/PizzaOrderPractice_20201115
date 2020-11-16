f396523cd0e39e04f2a529430129b03ed6cf520d
kimhaun <wha02068@gmail.com>
2020-11-16 20:24:24 +0900

    PizzaStoreAdapter.kt 작성
    값세팅은 아직안됨

--- app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/fragments/PizzaOrder.kt
+++ app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/fragments/PizzaOrder.kt
@@ -6,14 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kr.co.neoplus.pizzaorderpractice_20201115.R
+import kr.co.neoplus.pizzaorderpractice_20201115.adapters.PizzaStoreAdapter
+import kr.co.neoplus.pizzaorderpractice_20201115.datas.PizzaStore
 
 class PizzaOrder : Fragment() {
+    var pizzaStoreList = ArrayList<PizzaStore>()
+    lateinit var mPizzaStoreAdapter: PizzaStoreAdapter
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_pizza, container, false)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
 
     }
 }
