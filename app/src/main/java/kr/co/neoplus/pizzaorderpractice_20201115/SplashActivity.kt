f8980c78c6a96f238a55d1308e47ad7dcfbe4706
KIMHAUN <wha02068@gmailcom>
2020-11-17 16:37:41 +0900

    gift_animation loading 화면

--- app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/SplashActivity.kt
+++ app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/SplashActivity.kt
@@ -1,18 +1,31 @@
 package kr.co.neoplus.pizzaorderpractice_20201115
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
     }
 
     override fun setValues() {
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+            val myIntent = Intent(mContext, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }, 3000)
     }
 
 
