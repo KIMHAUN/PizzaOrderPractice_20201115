36e79ffda5b48df6108259218d92424cf21618d6
KIMHAUN <wha02068@gmailcom>
2020-11-17 16:30:02 +0900

    SplashActivity.kt 추가

---
+++ app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/SplashActivity.kt
@@ -0,0 +1,19 @@
+package kr.co.neoplus.pizzaorderpractice_20201115
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+
+}
