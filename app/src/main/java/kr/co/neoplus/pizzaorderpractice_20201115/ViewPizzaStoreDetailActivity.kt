4d23a0cba4f3b72d7c5c127f6ea8c7c6df0c93be
KIMHAUN <wha02068@gmailcom>
2020-11-18 16:17:36 +0900

    피자 가게 상세 화면 세팅

--- app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/ViewPizzaStoreDetailActivity.kt
+++ app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/ViewPizzaStoreDetailActivity.kt
@@ -2,6 +2,8 @@ package kr.co.neoplus.pizzaorderpractice_20201115
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_pizza_store_detail.*
+import kr.co.neoplus.pizzaorderpractice_20201115.datas.PizzaStore
 
 class ViewPizzaStoreDetailActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +17,9 @@ class ViewPizzaStoreDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        val pizzaStore = intent.getSerializableExtra("pizzaStoreData") as PizzaStore
+       // storeNameTxt =
+
     }
 }
