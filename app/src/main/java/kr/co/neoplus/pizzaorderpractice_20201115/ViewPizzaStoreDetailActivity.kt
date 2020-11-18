e7edf5bdc7865e31740866f8cf2b24addd85e1c2
KIMHAUN <wha02068@gmailcom>
2020-11-18 17:03:26 +0900

    상세화면 정보 세팅

--- app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/ViewPizzaStoreDetailActivity.kt
+++ app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/ViewPizzaStoreDetailActivity.kt
@@ -2,6 +2,7 @@ package kr.co.neoplus.pizzaorderpractice_20201115
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_view_pizza_store_detail.*
 import kr.co.neoplus.pizzaorderpractice_20201115.datas.PizzaStore
 
@@ -19,7 +20,10 @@ class ViewPizzaStoreDetailActivity : BaseActivity() {
     override fun setValues() {
 
         val pizzaStore = intent.getSerializableExtra("pizzaStoreData") as PizzaStore
-       // storeNameTxt =
+        storeNameTxt.text = pizzaStore.name
+        phoneNoTxt.text = pizzaStore.phoneNo
+        Glide.with(mContext).load(pizzaStore.logoURL).into(pizzaLogoImg)
+
 
     }
 }
