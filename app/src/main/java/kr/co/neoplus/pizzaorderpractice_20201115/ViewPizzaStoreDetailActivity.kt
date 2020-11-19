1ae610a3b22e0f0158b4a51fcb624df66295dc7a
KIMHAUN <wha02068@gmailcom>
2020-11-19 16:13:49 +0900

    피자 가게에 전화 걸기 완료

--- app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/ViewPizzaStoreDetailActivity.kt
+++ app/src/main/java/kr/co/neoplus/pizzaorderpractice_20201115/ViewPizzaStoreDetailActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.DEBUG
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_view_pizza_store_detail.*
+import kr.co.neoplus.pizzaorderpractice_20201115.BuildConfig.DEBUG
 import kr.co.neoplus.pizzaorderpractice_20201115.datas.PizzaStore
 
 class ViewPizzaStoreDetailActivity : BaseActivity() {
@@ -19,21 +23,8 @@ class ViewPizzaStoreDetailActivity : BaseActivity() {
         setupEvents()
     }
     override fun setupEvents() {
-        val pizzaStore = intent.getSerializableExtra("pizzaStoreData") as PizzaStore
-        callBtn.setOnClickListener {
-            val pl = object : PermissionListener{
-                override fun onPermissionGranted() {
-                    val myUri = Uri.parse(pizzaStore.phoneNo)
-                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
-                    startActivity(myIntent)
+       // val pizzaStore = intent.getSerializableExtra("pizzaStoreData") as PizzaStore
 
-                }
-
-                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
-                    Toast.makeText(mContext, "권한이 거부되어 통화연결이 불가능합니다.", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
 
     }
 
@@ -43,6 +34,27 @@ class ViewPizzaStoreDetailActivity : BaseActivity() {
         storeNameTxt.text = pizzaStore.name
         phoneNoTxt.text = pizzaStore.phoneNo
         Glide.with(mContext).load(pizzaStore.logoURL).into(pizzaLogoImg)
+
+        callBtn.setOnClickListener {
+            //Log.DEBUG("call button click")
+            val pl = object : PermissionListener{
+                override fun onPermissionGranted() {
+                    val myUri = Uri.parse("tel:"+pizzaStore.phoneNo)
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "권한이 거부되어 통화연결이 불가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            TedPermission.with(mContext)
+                    .setPermissionListener(pl)
+                    .setDeniedMessage("전화 권한을 거부하면 연결이 불가합니다.")
+                    .setPermissions(android.Manifest.permission.CALL_PHONE)
+                    .check()
+        }
 
 
     }
